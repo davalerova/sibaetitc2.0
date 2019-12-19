4b7704acae607a1799e0e59b6363bfc538bd54ee
davalerova <44624476+davalerova@users.noreply.github.com>
2019-12-19 11:04:51 -0500

    Se actualiza vista de login

--- des/forms.py
+++ des/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.db import *
 from ins.models import Inscripcion
+from datetime import date
 
 from .models import *
 from django.contrib.auth.models import User
@@ -8,13 +9,46 @@ from django.contrib.auth.models import User
 
 ########################################################################################################################
 class ConsumoForm( forms.ModelForm ):
-    inscripcion = forms.ModelChoiceField(
-        queryset=Inscripcion.objects.filter( estado=True )
-            .order_by( 'beneficiario' )
-    )
+    dia = date.weekday( date.today() )
+    if dia == 0:
+        inscripcion = forms.ModelChoiceField(
+            queryset=Inscripcion.objects.filter( servicio__lunes=True, estado=True, )
+                .order_by( 'beneficiario' )
+        )
+    elif dia == 1:
+        inscripcion = forms.ModelChoiceField(
+            queryset=Inscripcion.objects.filter( servicio__martes=True, estado=True )
+                .order_by( 'beneficiario' )
+        )
+    elif dia == 2:
+        inscripcion = forms.ModelChoiceField(
+            queryset=Inscripcion.objects.filter( servicio__miercoles=True, estado=True )
+                .order_by( 'beneficiario' )
+        )
+    elif dia == 3:
+        inscripcion = forms.ModelChoiceField(
+            queryset=Inscripcion.objects.filter( servicio__jueves=True, estado=True )
+                .order_by( 'beneficiario' )
+        )
+    elif dia == 4:
+        inscripcion = forms.ModelChoiceField(
+            queryset=Inscripcion.objects.filter( servicio__viernes=True, estado=True )
+                .order_by( 'beneficiario' )
+        )
+    elif dia == 5:
+        inscripcion = forms.ModelChoiceField(
+            queryset=Inscripcion.objects.filter( servicio__sabado=True, estado=True )
+                .order_by( 'beneficiario' )
+        )
+    else:
+        inscripcion = forms.ModelChoiceField(
+            queryset=Inscripcion.objects.filter( servicio__domingo=True, estado=True )
+                .order_by( 'beneficiario' )
+        )
+
     class Meta:
         model = Consumo
-        fields = ['inscripcion','estado']
+        fields = ['inscripcion', 'estado']
         labels = {'inscripcion': "Inscripcion", "estado": "Estado"}
         widget = {'estado': forms.BooleanField}
 
